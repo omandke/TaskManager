140c36b9c401388be04f93d94309a493a1aa2ee9
omandke <onkarmandke@gmail.com>
2025-04-02 22:19:29 +0530

    Refactor UserRepository to extend CrudRepository for simplified data access

--- src/main/java/com/task/repository/UserRepository.java
+++ src/main/java/com/task/repository/UserRepository.java
@@ -2,12 +2,12 @@ package com.task.repository;
 
 import java.util.Optional;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.task.entity.User;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, Long> {
     Optional<User> findUserByUsername(String username);
     Optional<User> findByEmail(String email);
     

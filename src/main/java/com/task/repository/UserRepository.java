0f25d0d11252ff8260502b85413aa55786cf5fcb
omandke <onkarmandke@gmail.com>
2025-04-02 21:31:35 +0530

    create user repository interface

---
+++ src/main/java/com/task/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.task.repository;
+
+import java.util.Optional;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByUsername(String username);
+    Optional<User> findByEmail(String email);
+    
+}

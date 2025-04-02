ce990cf5e61d2776a743fc6496a077797baaf1bc
omandke <onkarmandke@gmail.com>
2025-04-02 22:19:16 +0530

    Refactor User entity class: rename to 'User', update table name, and add constructors

--- src/main/java/com/task/entity/user.java
+++ src/main/java/com/task/entity/user.java
@@ -6,16 +6,18 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
+@Table(name = "users")
 @Getter
 @Setter
 @NoArgsConstructor
-@Entity
-@Table(name = "user")
-public class user {
+@AllArgsConstructor
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
